96dbb61b3ed8817e2abba6b5b2d871a434eef9de
Robert Lowe <rob@lowefrequency.com>
2016-12-05 22:22:06 -0500

    Main things, they don't work

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,16 @@
-class Main {
+import akka.actor.Props
+import akka.actor.ActorSystem
+import actors.DocumentCheck
+import messages.Document
 
+object Main {
+  def main(args: Array[String]): Unit = {
+    
+    val system = ActorSystem("mySystem")
+    val myActor = system.actorOf(Props(new DocumentCheck(null)), "docCheck")
+    
+    myActor ! new Document(false);
+    
+    println("Hello, world!")
+  }
 }
