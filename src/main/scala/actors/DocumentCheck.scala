fea18924ebd08a5cd903727e77d223413d3ba3ec
Daniel <dds2130@rit.edu>
2016-12-07 22:22:15 -0500

    reverted documentcheck

--- src/main/scala/actors/DocumentCheck.scala
+++ src/main/scala/actors/DocumentCheck.scala
@@ -3,44 +3,38 @@ package actors
 import akka.actor.{Actor, ActorRef}
 
 import scala.collection.mutable
-import messages.{Document, StartPerson}
-
+import messages.{Document, StartPerson,DocPassFail}
 import scala.collection.immutable.Queue
 
 
-//class DocumentCheck(people: Queue[ActorRef], n: Int) extends Actor {
-class DocumentCheck(n: Int) extends Actor{
-  val queues = mutable.MutableList[PersonQueue]()
-  var i = 0
-  for (i <- 0 until n) {
-    queues += new PersonQueue()
-  }
-  i = 0
+class DocumentCheck(queues: mutable.MutableList[ActorRef]) extends Actor{
 
-//  Starts all the people
-//  for (person <- people) person ! new StartPerson(self)
 
   def checkDocument(d: Document){
     if(d.isValid){
       println("Valid Document");
-//      sender() ! new DocPassFail(nextQueue, true);
+      sender() ! new DocPassFail(nextQueue, true);
     }
     else{
       println("Invalid Document");
-//      sender() ! new DocPassFail(null, false);
+      sender() ! new DocPassFail(null, false);
     }
   }
-  
+
   def receive = {
     case x : Document => checkDocument(x);
     case _ => ;
   }
 
+  var n = queues.length
+  var i = 0
 
-  def nextQueue = {
-    if (i >= n) i = 0
+  def nextQueue: ActorRef = {
+    if (i >= n){
+      i = 0
+    }
     val queue = queues.get(i)
     i += 1
-    queue //return
+    return queue.get
   }
 }
