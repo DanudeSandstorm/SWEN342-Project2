f5b39f53af3209b09b0c00a21380e008a5dbc015
Daniel <dds2130@rit.edu>
2016-12-07 23:07:41 -0500

    Securitygs

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,42 +1,23 @@
 import akka.actor.Props
 import akka.actor.ActorSystem
 import actors.DocumentCheck
-import messages.Document
 import akka.actor.PoisonPill
 import actors.Person
 import messages.StartPerson
-import actors.{PersonQueue, BagScan, BodyScan, Jail, Security}
-import scala.collection.mutable
-import akka.actor.ActorRef
 
 object Main {
-  val numberOfQueues = 10;
-  
   def main(args: Array[String]): Unit = {
     
     val system = ActorSystem("mySystem")
+    val documentCheck = system.actorOf(Props(new DocumentCheck(0)), "docCheck")
     
-    val jail = system.actorOf(Props(new Jail()));
-    val security = system.actorOf(Props(new Security(jail)));
-    
-    val queues = mutable.MutableList[ActorRef]()
-    for (i <- 0 until numberOfQueues) {
-      val bagScanner = system.actorOf(Props(new BagScan(security)));
-      val bodyScanner = system.actorOf(Props(new BodyScan(security)));
-      queues += system.actorOf(Props(new PersonQueue(bagScanner, bodyScanner)));
-    }
-    
-    val documentCheck = system.actorOf(Props(new DocumentCheck(queues)), "docCheck")
-    for(x <- 0 to 100){
-      
-      val person = system.actorOf(Props(new Person()), "Person".concat(x.toString()) );
+    for(x <- 0 to 29){
+      val person = system.actorOf(Props(new Person()));
       person ! new StartPerson(documentCheck);
-      
     }
     
-    Thread.sleep(100)
-    documentCheck ! PoisonPill
-    Thread.sleep(1900)
+    //documentCheck ! PoisonPill
+    Thread.sleep(5000)
     system.terminate()
     
   }
