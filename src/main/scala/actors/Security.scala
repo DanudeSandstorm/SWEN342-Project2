04b19f87dfd1dd6438858d5e8c4b9be24dc12af2
Daniel <dds2130@rit.edu>
2016-12-07 21:12:12 -0500

    merge

--- src/main/scala/actors/Security.scala
+++ src/main/scala/actors/Security.scala
@@ -1,11 +1,46 @@
 package actors
 
-import akka.actor.Actor
+import akka.actor.{Actor, ActorRef}
+import messages.{BagPassFail, BodyPassFail, PassFailMsg, Fly}
+import scala.collection.mutable.Queue
+
 
 class Security extends Actor {
 
+  var passed = new Queue[ActorRef]();
+
   def receive = {
+    case x: BagPassFail => bagChecked(x)
+    case x: BodyPassFail => bodyChecked(x)
     case _      => ()
+  }
+
+  def bagChecked(x: PassFailMsg) {
+    if (x.pass) {
+//      if (passed.find(x.actor_ref)) {
+//        pass(x.actor_ref)
+//      }
+
+      x.actor_ref.tell(self, self) //Sends self to queue
+    } else {
+      jail(x.actor_ref)
+    }
+  }
+
+  def bodyChecked(x: PassFailMsg) {
+    if (x.pass) {
+      x.actor_ref.tell(self, self) //Sends self to queue
+    } else {
+      jail(x.actor_ref)
+    }
+  }
+
+  def pass(x: ActorRef): Unit = {
+    x.tell(new Fly(),self)
+  }
+
+  def jail(x: ActorRef): Unit = {
+    //jail//.tell(new GoToJail(x));
   }
 
 }
